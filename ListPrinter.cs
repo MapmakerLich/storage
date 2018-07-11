ff5fcad40fb9b49a5d146770bb8e8e7af73b92c1
Михаил <32036274+MapmakerLich@users.noreply.github.com>
2018-07-11 20:38:10 +0300

    Create ListPrinter.cs

---
+++ ListPrinter.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListPrinter : MonoBehaviour {
+
+    [SerializeField]
+    public List<int> numbers = new List<int>() {1, 1, 1, 1};
+    private int number;
+    private int index;
+    private float currentTime = 0;
+    private float waitingTime = 5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > waitingTime)
+        {
+            Print();
+        }
+	}
+
+    void Print()
+    {
+        index = Random.Range(0, numbers.Count-1);
+        Debug.Log(numbers[index]);
+        currentTime = 0;
+    }
+}
