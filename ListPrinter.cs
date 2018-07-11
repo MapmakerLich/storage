3c1e9ec711a73f9af0bd58be4bc9427776ac807c
Михаил <32036274+MapmakerLich@users.noreply.github.com>
2018-07-11 21:10:22 +0300

    Update ListPrinter.cs

--- ListPrinter.cs
+++ ListPrinter.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 public class ListPrinter : MonoBehaviour {
 
     [SerializeField]
-    public List<int> numbers = new List<int>() {1, 1, 1, 1};
+    private List<int> numbers = new List<int>() {1, 1, 1, 1};
     private int number;
     private int index;
     private float currentTime = 0;
     private float waitingTime = 5f;
+    private int previousNumber;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
 	
-    // Update is called once per frame
-    void Update ()
+	// Update is called once per frame
+	void Update ()
     {
         currentTime += Time.deltaTime;
         if (currentTime > waitingTime)
-           {
-               Print();
-           }
+        {
+            previousNumber = numbers[numbers.Count-1];
+            Print();
+        }
 	}
 
     void Print()
     {
-        index = Random.Range(0, numbers.Count-1);
+        do
+        {
+            previousNumber = numbers[index];
+            index = Random.Range(0, numbers.Count);
+        }
+        while (previousNumber == numbers[index]);
         Debug.Log(numbers[index]);
         currentTime = 0;
     }
